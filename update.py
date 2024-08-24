008ec1ab5101fd785f69236ad07582555e5d18fb
FALGUN VAGHASHIYA <falgunpatel.fv@gmail.com>
2024-08-24 18:39:59 +0530

    Update update.py

--- update.py
+++ update.py
@@ -71,11 +71,11 @@ if DATABASE_URL is not None:
 
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+    UPSTREAM_REPO = "https://github.com/lordgaruda/Z"
 
 UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = "master"
+    UPSTREAM_BRANCH = "main"
 
 if UPSTREAM_REPO is not None:
     if path.exists(".git"):
